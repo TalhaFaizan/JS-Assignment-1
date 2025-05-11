846e097e96f1907ce1c537f338d83539daa546c2
Talha Faizan <140506077+TalhaFaizan@users.noreply.github.com>
2025-05-11 20:01:42 +0500

    JS-Assignment-1

--- script.js
+++ script.js
@@ -17,24 +17,6 @@ const formattedArray = [];
 for (let i = 0; i < words.length; i++) {
   let word = words[i];
   let formattedWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-
-  // Word Finder
-
-  let str = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae facere sapiente magni cum! Rerum ipsa corrupti animi? Quo odio sequi laboriosam quos suscipit possimus. Omnis a voluptate repellendus culpa eveniet?";
-
-let userfind = "omnis";
-let counter = 0;
-for(let i = 0;i<str.length;i++){
-    if(userfind==str.slice(i,i+userfind.length)){
-        ++counter;
-        console.log("match");
-        
-    }else{
-        console.log("Not Match");
-        
-    }
-
-}
   formattedArray.push(formattedWord);
 }
 console.log("Formatted Array:", formattedArray);
